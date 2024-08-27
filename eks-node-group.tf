419f3411d48350dd4a7a8b4211035418f118e114
Raymond Efa <ereta019@gmail.com>
2024-08-27 12:08:49 +0000

    Upload New File

---
+++ eks-node-group.tf
@@ -0,0 +1,25 @@
+# EKS Node Group
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "my-node-group2"
+  node_role_arn   = aws_iam_role.node_group_role-2.arn
+  subnet_ids      = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 4
+    min_size     = 2
+  }
+
+  ami_type       = "AL2_x86_64"
+  instance_types = ["t3.micro"]
+  capacity_type  = "ON_DEMAND"
+  disk_size      = 20
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_group_policy,
+    aws_iam_role_policy_attachment.node_group_cni_policy,
+    aws_iam_role_policy_attachment.node_group_registry_policy
+  ]
+}
